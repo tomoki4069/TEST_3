4c7aee4332de726942ad8573b00b40e46acc036c
tomoki4069 <63623907+tomoki4069@users.noreply.github.com>
2020-05-11 15:16:50 +0900

    Revert "[Update]インスタンス内の処理を変更しました"

--- resistance_check.py
+++ resistance_check.py
@@ -8,38 +8,8 @@ from tkinter import messagebox
 
 class Application(tk.Frame):
     def __init__(self, master=None):
-        self.labelBoard = tk.Label(win, text=u'Resistance display:')
-        self.labelBoard.place(x=120,y=20)
+        def x():
 
-        self.Resistance_EditBox_1 = tk.Entry(width=10)
-        self.Resistance_EditBox_1.insert(tk.END,"1234")
-        self.Resistance_EditBox_1.insert(tk.END, '0')
-        self.Resistance_EditBox_1.place(x=120,y=40)
-
-        self.labelResistance = tk.Label(win, text=u'Resistance values:')
-        self.labelResistance.place(x=250,y=20)
-
-        self.labelResult = tk.Label(win, text=u'---')
-        self.labelResult.place(x=250,y=40)
-
-        self.calcButton = tk.Button(win, text=u'check',command= self.x)#ここにコマンドを追加する。それにより描画時以外でもボタン処理が可能
-        #self.calcButton["command"] = self.x() # この処理だと描画した時だけ処理されるので別の方法で行う
-        self.calcButton.place(x=220,y=80)
-
-        self.labelResistance = tk.Label(win, text=u'Precision:')
-        self.labelResistance.place(x=180,y=130)
-
-        self.combo = ttk.Combobox(win, width=4)
-        self.combo["values"] = ("F","A","B")
-        self.combo.place(x=180,y=150)
-
-        self.labelResistance = tk.Label(win, text=u'Size:')
-        self.labelResistance.place(x=250,y=130)
-
-        self.combo = ttk.Combobox(win, width=4)
-        self.combo["values"] = ("0603","1608","2125")
-        self.combo.place(x=250,y=150)
-    def x(self):
             a=self.Resistance_EditBox_1.get()
             b=int(a[-1])
             c=int(a[0:-1])
@@ -61,14 +31,44 @@ class Application(tk.Frame):
 
             else:
                 f=str(d)
-                self.labelResult['text'] = f+"[ohm]"  
-    def Vol_Turn_Off(self):
-        messagebox.askokcancel("Quit", "眠い？")
+                self.labelResult['text'] = f+"[ohm]"
+
+        self.labelBoard = tk.Label(win, text=u'Resistance display:')
+        self.labelBoard.place(x=120,y=20)
+
+        self.Resistance_EditBox_1 = tk.Entry(width=10)
+        self.Resistance_EditBox_1.insert(tk.END, '0')
+        self.Resistance_EditBox_1.place(x=120,y=40)
+
+        self.labelResistance = tk.Label(win, text=u'Resistance values:')
+        self.labelResistance.place(x=250,y=20)
+
+        self.labelResult = tk.Label(win, text=u'---')
+        self.labelResult.place(x=250,y=40)
+
+        self.calcButton = tk.Button(win, text=u'check')
+        self.calcButton["command"] = x
+        self.calcButton.place(x=220,y=80)
+
+        self.labelResistance = tk.Label(win, text=u'Precision:')
+        self.labelResistance.place(x=180,y=130)
+
+        self.combo = ttk.Combobox(win, width=4)
+        self.combo["values"] = ("F","A","B")
+        self.combo.place(x=180,y=150)
+
+        self.labelResistance = tk.Label(win, text=u'Size:')
+        self.labelResistance.place(x=250,y=130)
+
+        self.combo = ttk.Combobox(win, width=4)
+        self.combo["values"] = ("0603","1608","2125")
+        self.combo.place(x=250,y=150)
+       
 win = tk.Tk()
 win.title("Resistance conversion")
 win.geometry("500x500")
 win.resizable(0,0)
-#win.resizable(False, False)
+win.resizable(False, False)
 app = Application(master=win)
 def on_closing():
         try:
